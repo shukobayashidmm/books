1a2ee0b0e32828f24e6161a5c672f4f12f5982d0
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-06 08:59:18 +0000

    start-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
   get 'books/top'
-  get 'homes/top'
+  get '/top' => 'homes#top'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
