4b7cc5555af26d4d86866e24de60d67b329dac7b
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-06 10:21:41 +0000

    started

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new
+    book.save
+    redirect_to '/top'
   end
 
   def index
