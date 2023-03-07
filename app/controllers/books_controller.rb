2153d113bfbcf9faf2befc03498c98a51efc1296
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-07 23:59:02 +0000

    finished

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,14 @@ class BooksController < ApplicationController
 
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
