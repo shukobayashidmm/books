fe299a4ed2ae69114b97721c698f1d1bd47d6ea6
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-07 07:39:01 +0000

    2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,35 @@
 class BooksController < ApplicationController
-  def top
+
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+   @book = Book.find(params[:id])
+   book = Book.find(params[:id])
+  end
+
+ def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to '/books'
+ end
+
+  def edit
+  end
+
+
+ private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
