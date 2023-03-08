ec79cfe4c1ca94a634eced1126222f23032a987e
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-08 06:05:32 +0000

    2-finished

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,12 @@ class BooksController < ApplicationController
 
 
   def create
-    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -19,7 +19,6 @@ class BooksController < ApplicationController
 
   def show
    @book = Book.find(params[:id])
-   book = Book.find(params[:id])
   end
 
  def destroy
@@ -33,9 +32,13 @@ class BooksController < ApplicationController
  end
 
 def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+  @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
 end
 
  private
